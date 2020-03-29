a76e0b9bbc9d6df3d5bf2838b7a0b26e39e73897
Ferhat TEKKOUK <38411423+Ferroxy@users.noreply.github.com>
2020-03-29 03:30:26 +0100

    Add files via upload

---
+++ fibonnaci.py
@@ -0,0 +1,15 @@
+def fibonnaci(n):
+    #la suite de fibonnaci
+    a, b, c = 1, 1, 0
+    while (c < n):
+        print(f'{c+1:2} _ {a:8} : {b:9}')
+        a, b, c = b, a+b, c+1
+        
+def fibon(n):
+    #la suite de fibonnaci
+    a, b, c = 1, 1, 0
+    s = 'f_ibon('+ str(n) + ') = '
+    while (c < n):
+        a, b, c = b, a+b, c+1
+    s = s + str(a)
+    print(s)
